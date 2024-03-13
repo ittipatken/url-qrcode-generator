0119163600aee3e845fd7018f52f4d511adde6b0
ittipat <39715559+ittipatken@users.noreply.github.com>
2024-03-14 04:31:19 +0700

    set trustHost to true

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import type { NextAuthConfig } from "next-auth";
 import NextAuth from "next-auth";
 
 export const config = {
+  trustHost: true,
   providers: [Google],
   callbacks: {
     async redirect({ baseUrl }) {
